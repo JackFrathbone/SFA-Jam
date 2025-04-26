e8e494515a7f70687e1072a9010885f679a56995
Jack <183477472+jack-yl@users.noreply.github.com>
2025-04-26 16:35:57 +0200

    Bee

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,6 +17,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _thrustForce = 5f;
     [SerializeField] private float _rotationSpeed = 200f;
     [SerializeField] private float _maxVelocity = 10f;
+
+    [SerializeField] List<Sprite> _shipSprites = new();
 
     [Header("References")]
     [SerializeField] private GameObject _frontFire;
@@ -50,12 +53,15 @@ public class PlayerController : MonoBehaviour
         switch (_playerNumber)
         {
             case PlayerNumber.one:
+                _spriteRenderer.sprite = _shipSprites[0];
                 _spriteRenderer.color = Color.green;
                 break;
             case PlayerNumber.two:
+                _spriteRenderer.sprite = _shipSprites[1];
                 _spriteRenderer.color = Color.red;
                 break;
             case PlayerNumber.three:
+                _spriteRenderer.sprite = _shipSprites[2];
                 _spriteRenderer.color = Color.blue;
                 break;
             case PlayerNumber.four:
